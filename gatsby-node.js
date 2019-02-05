cc818005cd930b017921a89cbe25dae6bac244e5
Ludo Bonnet <lbonnet84@gmail.com>
2019-02-05 14:54:32 +0100

    no message

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,9 @@
 "use strict"
 
+const fs = require(`fs-extra`)
+
+const array = require(`lodash/array`)
+
 const { search } = require(`./youtube`)
 
 exports.sourceNodes = (
@@ -19,6 +23,40 @@ exports.sourceNodes = (
     globalOptions.maxVideos = maxVideos
   }
 
+  const dir = `./src/data/videos`
+
+  const concatVideos = options =>
+    new Promise(async (resolve, reject) => {
+      try {
+        const file = `${dir}/${options.channelId}.json`
+        let oldContent = null
+        await fs.ensureFile(file)
+        oldContent = await fs.readJson(file, {
+          throws: false,
+        })
+        let content = await search(options)
+        let old = 0
+
+        if (oldContent !== null) {
+          old = oldContent.length
+          content = array.unionBy(content, oldContent, `id`)
+        }
+
+        const total = content.length
+
+        if (total >= 1) {
+          console.info(`from ${content[0].channelTitle}`)
+        }
+
+        console.info(`New: ${total - old}`)
+        console.info(`Total: ${total}`)
+        resolve(content)
+      } catch (err) {
+        console.error(err)
+        reject(err)
+      }
+    })
+
   const processVideo = video => {
     const nodeId = createNodeId(`YoutubeVideo-${video.id}`)
     const nodeContent = JSON.stringify(video)
@@ -35,14 +73,18 @@ exports.sourceNodes = (
     return nodeData
   }
 
-  return Promise.all(
+  Promise.all(
     searches.map(async options => {
-      const opts = Object.assign(globalOptions, options)
-      const results = await search(opts)
-      return results.map(result => {
-        const nodeData = processVideo(result)
-        return createNode(nodeData)
-      })
+      try {
+        const opts = Object.assign({}, globalOptions, options)
+        const results = await concatVideos(opts)
+        results.map(result => {
+          const nodeData = processVideo(result)
+          createNode(nodeData)
+        })
+      } catch (err) {
+        console.error(err)
+      }
     })
   )
 }
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,3 +1,6 @@
+const fs = require(`fs-extra`)
+const array = require(`lodash/array`)
+
 const { search } = require(`./youtube`)
 
 exports.sourceNodes = (
@@ -17,6 +20,34 @@ exports.sourceNodes = (
     globalOptions.maxVideos = maxVideos
   }
 
+  const dir = `./src/data/videos`
+
+  const concatVideos = options =>
+    new Promise(async (resolve, reject) => {
+      try {
+        const file = `${dir}/${options.channelId}.json`
+        let oldContent = null
+        await fs.ensureFile(file)
+        oldContent = await fs.readJson(file, { throws: false })
+        let content = await search(options)
+        let old = 0
+        if (oldContent !== null) {
+          old = oldContent.length
+          content = array.unionBy(content, oldContent, `id`)
+        }
+        const total = content.length
+        if (total >= 1) {
+          console.info(`from ${content[0].channelTitle}`)
+        }
+        console.info(`New: ${total - old}`)
+        console.info(`Total: ${total}`)
+        resolve(content)
+      } catch (err) {
+        console.error(err)
+        reject(err)
+      }
+    })
+
   const processVideo = video => {
     const nodeId = createNodeId(`YoutubeVideo-${video.id}`)
     const nodeContent = JSON.stringify(video)
@@ -34,15 +65,19 @@ exports.sourceNodes = (
     return nodeData
   }
 
-  return Promise.all(
+  Promise.all(
     searches.map(async options => {
-      const opts = Object.assign(globalOptions, options)
-      const results = await search(opts)
+      try {
+        const opts = Object.assign({}, globalOptions, options)
 
-      return results.map(result => {
-        const nodeData = processVideo(result)
-        return createNode(nodeData)
-      })
+        const results = await concatVideos(opts)
+        results.map(result => {
+          const nodeData = processVideo(result)
+          createNode(nodeData)
+        })
+      } catch (err) {
+        console.error(err)
+      }
     })
   )
 }
